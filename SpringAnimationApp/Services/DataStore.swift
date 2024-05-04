b78d2f905cefc2737fd8111877483c48926dd550
Gordey Guselnikov <g.guselnikov@bi.zone>
2024-05-04 23:21:02 +0700

    fixes2

--- SpringAnimationApp/Services/DataStore.swift
+++ SpringAnimationApp/Services/DataStore.swift
@@ -5,4 +5,43 @@
 //  Created by Guselnikov Gordey on 6/16/23.
 //
 
-import Foundation
+import SpringAnimation
+
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let animations: [AnimationPreset] = [
+        .fadeIn,
+        .fadeInDown,
+        .fadeInLeft,
+        .fadeInRight,
+        .fadeInUp,
+        .fadeOut,
+        .fadeOutIn,
+        .fall,
+        .flash,
+        .flipX,
+        .flipY,
+        .morph,
+        .pop,
+        .shake,
+        .slideDown,
+        .slideLeft,
+        .slideRight,
+        .slideUp,
+        .squeeze,
+        .squeezeDown,
+        .squeezeLeft,
+        .squeezeRight,
+        .squeezeUp,
+        .swing,
+        .zoomIn,
+        .wobble,
+        .zoomOut
+    ]
+    
+    let curves = AnimationCurve.allCases
+    
+    private init() {}
+}
--- SpringAnimationApp/ViewControllers/ViewController.swift
+++ SpringAnimationApp/ViewControllers/ViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 import SpringAnimation
 
 final class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var animationView: SpringView!
+    @IBOutlet weak var animationLabel: SpringLabel! {
+        didSet {
+            animationLabel.text = animation.description
+        }
+    }
+    
+    // MARK: - Private properties
+    private var animation = Animation.getRandomAnimation()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - IB Actions
+    @IBAction func animationButtonPressed(_ sender: UIButton) {
+        animationLabel.animation = "zoomOut"
+        animationLabel.x = 50
+        animationLabel.animate()
+        animationLabel.text = animation.description
         
+        animationLabel.animateNext { [unowned self] in
+            animationLabel.animation = "zoomIn"
+            animationLabel.animate()
+            
+            animationView.animation = animation.name
+            animationView.curve = animation.curve
+            animationView.duration = animation.duration
+            animationView.force = animation.force
+            animationView.delay = animation.delay
+            animationView.animate()
+            
+            animation = Animation.getRandomAnimation()
+            sender.setTitle("Run \(animation.name)", for: .normal)
+        }
     }
 }
-
