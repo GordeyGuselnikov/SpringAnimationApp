b924a2e22d2fd7fedab93718393e2407c1eb2498
Gordey Guselnikov <g.guselnikov@bi.zone>
2024-05-04 23:20:28 +0700

    fixes

---
+++ SpringAnimationApp/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  SpringAnimationApp
+//
+//  Created by Guselnikov Gordey on 6/16/23.
+//
+
+import Foundation
---
+++ SpringAnimationApp/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  SpringAnimationApp
+//
+//  Created by Guselnikov Gordey on 6/16/23.
+//
+
+import Foundation
